502bbbc7829bb4ccb324bf99e3e91283eda374d5
Jean Santos <jeansantosc@gmail.com>
2018-07-08 23:04:53 -0300

    add forecast.io api call | print current weather and temperature at address provided

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
@@ -19,6 +20,20 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    const key = 'f5fbeeb7aeec40d039f66b8feaec953f';
+    let lat = results.latitude;
+    let lng = results.longitude;
+
+    request({
+      url: `https://api.darksky.net/forecast/${key}/${lat},${lng}`,
+      json: true
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log(`Weather: ${body.currently.summary}`);
+        console.log(`Temperature: ${body.currently.temperature}`);
+      } else {
+        console.log('Unable to fetch weather.');
+      }
+    });
   }
 });
