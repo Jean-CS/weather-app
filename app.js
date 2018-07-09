16bcebc0667c4a8d1bf2f88d304e4ea03c11f584
Jean Santos <jeansantosc@gmail.com>
2018-07-08 21:58:18 -0300

    error checking

--- app.js
+++ app.js
@@ -22,10 +22,16 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true,
 }, (error, response, body) => {
-  let result = body.results[0];
-  let location = result.geometry.location;
+  if (error) {
+    console.log('Unable to connect to google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address');
+  } else if (body.status === 'OK'){
+    let result = body.results[0];
+    let location = result.geometry.location;
 
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${location.lat}`);
-  console.log(`Latitude: ${location.lng}`);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${location.lat}`);
+    console.log(`Latitude: ${location.lng}`);
+  }
 });
