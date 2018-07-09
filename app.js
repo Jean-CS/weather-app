76947fe62f46b2dd29860a10f26ca4126d379043
Jean Santos <jeansantosc@gmail.com>
2018-07-08 21:17:02 -0300

    request to google api and print response

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const request = require('request');
 
+// arg1: options object, configs
+// arg2: callback. will be called when data comes back from the http endpoint
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=rua%20dom%20fernando%20109%20londrina',
+  json: true,
+}, (error, response, body) => {
+  console.log(JSON.stringify(body, undefined, 2));
+});
